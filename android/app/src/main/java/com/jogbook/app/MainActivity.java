33b8cc83772f5c6f33565f71cf36234786612797
jogbook <chilstamusic@gmail.com>
2026-04-21 11:38:03 +0000

    fix signup flow

---
+++ android/app/src/main/java/com/jogbook/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.jogbook.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jogbook.app',
+  appName: 'Jogbook',
+  webDir: 'dist'
+};
+
+export default config;
